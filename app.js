92de3677c5395ce3810294d16dd14d9aa22826dc
yeonju <keeyeonju@gmail.com>
2020-07-17 02:07:41 +0900

    Access-Control-Allow-Headers 수정

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'content-type, x-access-token');
+  res.header('Access-Control-Allow-Headers', 'content-type,token,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization');
   next();
 });
 
